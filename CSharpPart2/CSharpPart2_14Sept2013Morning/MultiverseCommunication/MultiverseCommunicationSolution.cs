bc21e20c50c03989ffebd78f2dd6a5126b77560e
Doncho Minkov <donchominkov@gmail.com>
2015-02-28 10:59:13 +0200

    Added Multiverse Communications solution

---
+++ CSharpPart2/CSharpPart2_14Sept2013Morning/MultiverseCommunication/MultiverseCommunicationSolution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+
+namespace MultiverseCommunication
+{
+	class MultiverseCommunicationSolution
+	{
+
+		const int MultiverseSystemBase = 13;
+
+		static int GetDigitValue (string digit)
+		{
+			switch (digit) 
+			{
+			case "CHU": return 0;
+			case "TEL": return 1;
+			case "OFT": return 2;
+			case "IVA": return 3;
+			case "EMY": return 4;
+			case "VNB": return 5;
+			case "POQ": return 6;
+			case "ERI": return 7;
+			case "CAD": return 8;
+			case "K-A": return 9;
+			case "IIA": return 10;
+			case "YLO": return 11;
+			case "PLA": return 12;
+			default:
+				throw new ArgumentOutOfRangeException ("Invalid digit");
+			}
+		}
+
+
+		public static void Main ()
+		{
+			string multiverseNumber = Console.ReadLine ();
+			BigInteger decNumber = 0;
+			BigInteger power = 1;
+			for (int i = multiverseNumber.Length - 3; i >= 0; i-=3) 
+			{
+				string digit = multiverseNumber.Substring (i, 3);
+				int digitValue = GetDigitValue (digit);
+				decNumber += power * digitValue;
+				power *= MultiverseSystemBase;
+			}
+			Console.WriteLine (decNumber);
+		}
+	}
+}
