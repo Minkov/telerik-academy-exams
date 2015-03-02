56d2c67add9109b1c919903eb84e8b3df0ed9863
Doncho Minkov <donchominkov@gmail.com>
2015-03-02 18:52:42 +0200

    Added Solution to Joro the Rabbit

---
+++ CSharpPart2/C#Part2_05Feb2013/JoroTheRabbit/JoroTheRabbitSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace JoroTheRabbit
+{
+	class JoroTheRabbitSolution
+	{
+		public static void Main ()
+		{
+			Solve ();
+		}
+
+		public static void Solve ()
+		{
+			int[] terrain = Console.ReadLine ()
+								    .Split (new char[]{ ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+									.Select (int.Parse)
+									.ToArray ();
+			int max = 0;
+			for (int startPos = 0; startPos < terrain.Length; startPos++) {
+				for (int step = 1; step <= terrain.Length; step++) {
+					int lenght = 1;
+					int pos = startPos;
+					int prev = terrain [pos];
+//					bool[] isVisited = new bool[terrain.Length];
+
+					while (true) 
+					{
+						pos += step;
+						pos %= terrain.Length;
+						int next = terrain [pos];
+
+//						if (next <= prev || isVisited [pos]) 
+						if (next <= prev) 
+						{
+							break;
+						}
+
+//						isVisited [pos] = true;
+						prev = next;
+						++lenght;
+					}
+					if (max < lenght) {
+						max = lenght;
+					}
+				}
+			}
+			Console.WriteLine (max);
+		}
+	}
+}
---
+++ CSharpPart2/C#Part2_4Feb2013Morning/GreedyDrarf/GreedyDratfSolution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GreedyDrarf
+{
+	class GreedyDratfSolution
+	{
+		static string testInput = @"1, 3, -6, 7, 4 ,1, 12
+3
+1, 2, -3
+1, 3, -2
+1, -1";
+
+		public static void Main ()
+		{
+//			StringReader reader = new StringReader (testInput);
+//			Console.SetIn (reader);
+
+			char[] separators = { ' ', ',' };
+			int[] valley = Console.ReadLine ()
+							      .Split (separators, StringSplitOptions.RemoveEmptyEntries)
+								  .Select (int.Parse)
+							 	  .ToArray ();
+			int n = int.Parse (Console.ReadLine ());
+			bool isFirstCoins = true;
+			int max = 0;
+			for (int i = 0; i < n; i++) 
+			{
+				int[] pattern = Console.ReadLine ()
+									   .Split (separators, StringSplitOptions.RemoveEmptyEntries)
+									   .Select (int.Parse )
+									   .ToArray ();
+				int index = 0;
+				int coins = 0;
+				int patternIndex = 0;
+				bool[] isVisited = new bool[valley.Length];
+				while (true) 
+				{
+					index += pattern [patternIndex];
+					patternIndex++;
+					patternIndex %= pattern.Length;
+					if (index < 0 || valley.Length <= index || isVisited[index]) 
+					{
+						break;
+					}
+					coins += valley [index];
+					isVisited [index] = true;
+				}
+				if (isFirstCoins) 
+				{
+					isFirstCoins = false;
+					max = coins;
+				}
+				else if (max < coins) 
+				{
+					max = coins;
+				}
+			}
+			Console.WriteLine (max);
+		}
+	}
+}
