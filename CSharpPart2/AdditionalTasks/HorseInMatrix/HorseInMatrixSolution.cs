bc6d39c6e164fd8564eefd9c22b656d7a6a7de90
Doncho Minkov <donchominkov@gmail.com>
2015-03-03 16:43:26 +0200

    Added solution to HorseInMatrix

--- CSharpPart2/AdditionalTasks/HorseInMatrix/HorseInMatrixSolution.cs
+++ CSharpPart2/AdditionalTasks/HorseInMatrix/HorseInMatrixSolution.cs
@@ -4,21 +4,22 @@ namespace HorseInMatrix
 {
 	public class HorseInMatrixSolution
 	{
-		static bool InRange(int value, int max)
+		static bool InRange (int value, int max)
 		{
 			return 0 <= value && value <= max;
 		}
 
-		static void PrintMatrix(int[,] matrix)
+		static void PrintMatrix (int[,] matrix)
 		{
-			for (int row = 0; row < matrix.GetLength(0); row++) {
-				for (int col = 0; col < matrix.GetLength(1); col++) {
-					Console.Write ("{0} ", matrix[row, col]);
+			for (int row = 0; row < matrix.GetLength (0); row++) {
+				for (int col = 0; col < matrix.GetLength (1); col++) {
+					Console.Write ("{0} ", matrix [row, col]);
 				}
 			}
 		}
 
-		public static void Solve(){
+		public static void Solve ()
+		{
 			int[] dRows = { -2, -2, -1, -1, +1, +1, +2, +2 };
 			int[] dCols = { -1, +1, -2, +2, -2, +2, -1, +1 };
 
@@ -31,16 +32,15 @@ namespace HorseInMatrix
 
 			int[,] matrix = new int[n, n];
 
-			for (int count = 1; count <= n*n; count++) {
+			for (int count = 1; count <= n * n; count++) {
 				matrix [row, col] = count;
 				bool isMoveMade = false;
 				for (int dir = 0; dir < dRows.Length; dir++) {
 					int newRow = row + dRows [dir];
 					int newCol = col + dCols [dir];
-					if (InRange (newRow, n - 1) && 
-						InRange (newCol, n - 1) &&
-						matrix [newRow, newCol] == 0) 
-					{
+					if (InRange (newRow, n - 1) &&
+					    InRange (newCol, n - 1) &&
+					    matrix [newRow, newCol] == 0) {
 						row = newRow;
 						col = newCol;
 						isMoveMade = true;
@@ -51,13 +51,11 @@ namespace HorseInMatrix
 				if (!isMoveMade) {
 					bool isPositionFound = false;
 					for (int r = lastStartRow; r < n; r++) {
-						if (isPositionFound) 
-						{
+						if (isPositionFound) {
 							break;
 						}
 						for (int c = lastStartCol; c < n; c++) {
-							if (matrix [r, c] == 0) 
-							{
+							if (matrix [r, c] == 0) {
 								row = r;
 								col = c;
 								isPositionFound = true;
