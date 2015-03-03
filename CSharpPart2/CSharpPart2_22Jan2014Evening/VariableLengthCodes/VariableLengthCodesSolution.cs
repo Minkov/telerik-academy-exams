6fc8bf2c96afc36d0bbcf6d0c843a6fd81dc297e
Doncho Minkov <donchominkov@gmail.com>
2015-03-04 00:40:37 +0200

    Added solution to the Digits task

---
+++ CSharpPart2/CSharpPart2_22Jan2014Evening/VariableLengthCodes/VariableLengthCodesSolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariableLengthCodes
+{
+	class VariableLengthCodesSolution
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
