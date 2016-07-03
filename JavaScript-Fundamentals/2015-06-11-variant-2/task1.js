64b0603ce245aec2663c1042c9fa6446c7d2011b
Doncho Minkov <DonchoMinkov@gmail.com>
2016-07-03 18:33:15 +0300

    Task 1

--- JavaScript-Fundamentals/2015-06-11-variant-2/task1.js
+++ JavaScript-Fundamentals/2015-06-11-variant-2/task1.js
@@ -1,25 +1,24 @@
 function solve(args) {
-    let arr = args[0].split(' ').map(Number);
+    let initialHeights = args[0].split(' ').map(Number);
 
-    console.log(Math.max(findMaxRocks(arr), findMaxRocks(arr, true)));
+    console.log(Math.max(findMaxRocks(initialHeights), findMaxRocks(initialHeights, true)));
 
-    function findMaxRocks(arr, isUp) {
+    function findMaxRocks(heights, isUp) {
         let current,
             count = 0,
             best = -1;
 
         isUp = !!isUp;
-        [current, ...arr] = arr;
-        
-        for (let height of arr) {
+        [current, ...heights] = heights;
+
+        for (let height of heights) {
             if (isUp) {
                 if (current >= height) {
                     isUp = !isUp;
                     best = Math.max(best, count);
                     count = 0;
                 }
-            }
-            else {
+            } else {
                 if (current <= height) {
                     isUp = !isUp;
                 }
